11469f0810d0fbb9f985ca057f50b19f37f7a831
phillips <jshp2413@gmail.com>
2015-03-21 01:34:02 +0000

    Skeletal mock numerical data source base class.

--- src/servlet/core/AbstractDataSourceConnector.java
+++ src/servlet/core/AbstractDataSourceConnector.java
@@ -1,3 +1,21 @@
+/**
+  * This file is part of web-charts, an interactive web charts program.
+  *
+  * Copyright (C) 2015 John Kieran Phillips
+  * 
+  * web-charts is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  * 
+  * web-charts is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  * 
+  * You should have received a copy of the GNU General Public License
+  * along with web-charts.  If not, see <http://www.gnu.org/licenses/>.
+  */
 package servlet.core;
 
 import com.google.common.base.Preconditions;
