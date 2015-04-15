61b1b73229bfbc0700534e6a8d1290f716fe1436
phillips <jshp2413@gmail.com>
2015-04-15 22:19:16 +0100

    Adding set_configuration request processing to admin servlet.

--- src/servlet/core/AdminServlet.java
+++ src/servlet/core/AdminServlet.java
@@ -30,8 +30,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import servlet.core.AdminConfigurationInstruction.AdminConfigurationInstructionException;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -95,6 +98,38 @@ public final class AdminServlet extends HttpServlet {
    }
    
    /**
+     * A struct class used by GSON to parse a JSON <code>set_configuration</code> request.<br><br>
+     * 
+     * See also {@link AdminServlet#doPost(HttpServletRequest, HttpServletResponse)}.
+     * 
+     * @author phillips
+     */
+   private static class ConfigurationRequest {
+      private String
+         mode,
+         configuration;
+      
+      AdminConfigurationInstruction createConfiguration() {
+         switch(this.mode) {
+         case "live":
+            return (new Gson()).fromJson(
+               this.configuration, ConfigureLiveReceiverInstruction.class);
+         case "csv":
+            return null;
+         case "random":
+            return null;
+         default:
+            {
+            final String
+               errMsg = this.mode + " is not a valid operation mode.";
+            logger.error(errMsg);
+            throw new IllegalArgumentException(errMsg);
+            }
+         }
+      }
+   }
+   
+   /**
      * The POST response generator. This Servlet responds as follows:
      * 
      * <ul>
@@ -143,8 +178,8 @@ public final class AdminServlet extends HttpServlet {
       
       final Map<String, String[]>
          parameters = request.getParameterMap();
-      final Map<String, String>
-         responseMap = new HashMap<String, String>();
+      final Map<String, Object>
+         responseMap = new HashMap<String, Object>();
       
       if(parameters.isEmpty() || parameters.size() != 1) {
          logger.error("request is malformed.");
@@ -158,11 +193,31 @@ public final class AdminServlet extends HttpServlet {
       final String[]
          values = record.getValue();
       
-      if(name.equals("is_configured")) {
+      switch(name) {
+      case "is_configured":
          final String
             result = Boolean.toString(this.isConfigured);
          logger.info("ask: is_configured, response: " + result);
          responseMap.put("configuration_state", result);
+         break;
+      case "set_configuration":
+         final ConfigurationRequest
+            configurationRequest = this.gson.fromJson(values[0], ConfigurationRequest.class);
+         try {
+            final AdminConfigurationInstruction
+               instruction = configurationRequest.createConfiguration();
+            instruction.configure(super.getServletContext());
+            responseMap.put("configuration_result", true);
+         }
+         catch(final IllegalArgumentException | AdminConfigurationInstructionException e) {
+            logger.error(
+               "failed to create and execute configuration instruction from "
+             + "set_configuration request: " + ExceptionUtils.getRootCauseMessage(e) + ".");
+            responseMap.put("configuration_result", false);
+         }
+         break;
+      default:
+         
       }
       
       response.getWriter().write(this.gson.toJson(responseMap));
